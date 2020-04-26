d38392f8f00021caffb8c47499b0b5394a008793
orabaev <orabaev@gmail.com>
2020-04-26 16:56:42 -0400

    add convert::to_not_temporal_type

--- src/zman_temporal_convert.hpp
+++ src/zman_temporal_convert.hpp
@@ -13,15 +13,34 @@ struct temporal_convert
     using key_type                    = KEY;
     using value_type                  = VALUE; 
     using attribute                   = attribute<timepoint_type, value_type>; 
+    using attribute_type              = typename attribute::type; 
+    using attribute_temporal_type     = typename attribute::temporal_type; 
     using temporal_value_type         = typename attribute::temporal_value_type ;
     using array_type                  = typename attribute::array_type;
     using temporal_array_type         = typename attribute::temporal_array_type;
     using attribute_map               = attribute_map<timepoint_type, key_type, value_type>;
     using attribute_map_type          = typename attribute_map::type;
     using attribute_map_temporal_type = typename attribute_map::temporal_type;
+    
+    
+    
+    static std::optional<value_type> to_non_temporal_type(
+          const timepoint_type&  timepoint
+        , const value_type&      value
+    )
+    {
+        return std::optional<value_type>(value);
+    }
 
+    static std::optional<array_type> to_non_temporal_type(
+          const timepoint_type&  timepoint
+        , const array_type&      value
+    )
+    {
+        return std::optional<array_type>(value);
+    }
 
-    static std::optional<value_type> to_value_type(
+    static std::optional<value_type> to_non_temporal_type(
           const timepoint_type&      timepoint
         , const temporal_value_type& temporal_value
     )
@@ -30,7 +49,7 @@ struct temporal_convert
         return ptr ? std::optional<value_type>(*ptr) : std::nullopt;
     }
 
-    static std::optional<array_type> to_array_type(
+    static std::optional<array_type> to_non_temporal_type(
           const timepoint_type&      timepoint
         , const temporal_array_type& temporal_array
     )
@@ -38,12 +57,25 @@ struct temporal_convert
         array_type array;
         for (auto& temporal_value : temporal_array)
         {
-            auto optional_value = to_value_type(timepoint, temporal_value);
+            auto optional_value = to_non_temporal_type(timepoint, temporal_value);
             if (optional_value) array.push_back(*optional_value); 
         } 
         
         return !array.empty() ? std::optional<array_type>(std::move(array)) : std::nullopt;
     }
+
+/*
+    static std::optional<attribute_type> to_attribute_type(
+          const timepoint_type&          timepoint
+        , const attribute_temporal_type& temporal_attribute
+    )
+    {
+          
+
+    } 
+*/
+    
+
 }; 
 
 }
--- src/zman_temporal_convert.t.cpp
+++ src/zman_temporal_convert.t.cpp
@@ -11,7 +11,23 @@ using value_type      = int;
 using convert         = temporal_convert<time_point_type, key_type, value_type>;
 using attribute_t     = typename convert::attribute; 
 
-TEST_CASE("temporal_convert.to_value_type")
+TEST_CASE("temporal_convert.to_non_temporal_type.value")
+{
+    attribute_t::timepoint_type timepoint = 1;
+    value_type                  value     = 12;
+    auto optional_value = convert::to_non_temporal_type(timepoint, value);
+    CHECK( *optional_value == value );
+}
+
+TEST_CASE("temporal_convert.to_non_temporal_type.array")
+{
+    attribute_t::timepoint_type timepoint = 1;
+    attribute_t::array_type     array{1,2,3};
+    auto optional_value = convert::to_non_temporal_type(timepoint, array);
+    CHECK( *optional_value == array );
+}
+
+TEST_CASE("temporal_convert.to_non_temporal_type.temporal_value")
 {
     attribute_t::temporal_value_type temporal_value;    
 
@@ -23,19 +39,19 @@ TEST_CASE("temporal_convert.to_value_type")
     temporal_value.insert(timepoint);
     
     timepoint = 1;
-    auto optional_value = convert::to_value_type(timepoint, temporal_value);
+    auto optional_value = convert::to_non_temporal_type(timepoint, temporal_value);
     CHECK( *optional_value == 10 ); 
 
     timepoint = 2;
-    optional_value = convert::to_value_type(timepoint, temporal_value);
+    optional_value = convert::to_non_temporal_type(timepoint, temporal_value);
     CHECK( *optional_value == 10 ); 
 
     timepoint = 3;
-    optional_value = convert::to_value_type(timepoint, temporal_value);
+    optional_value = convert::to_non_temporal_type(timepoint, temporal_value);
     CHECK_FALSE( optional_value ); 
 }
 
-TEST_CASE("temporal_convert.to_array_type")
+TEST_CASE("temporal_convert.to_non_temporal_type.temporal_array")
 {
     attribute_t::temporal_array_type temporal_array;    
     attribute_t::temporal_value_type temporal_value;
@@ -60,22 +76,22 @@ TEST_CASE("temporal_convert.to_array_type")
     }
     
     timepoint = 1;
-    auto optional_value = convert::to_array_type(timepoint, temporal_array);
+    auto optional_value = convert::to_non_temporal_type(timepoint, temporal_array);
     CHECK( (*optional_value).size() ==  1 ); 
     CHECK( (*optional_value)[0]     == 10 ); 
 
     timepoint = 2;
-    optional_value = convert::to_array_type(timepoint, temporal_array);
+    optional_value = convert::to_non_temporal_type(timepoint, temporal_array);
     CHECK( (*optional_value).size() ==  2 ); 
     CHECK( (*optional_value)[0]     == 10 );
     CHECK( (*optional_value)[1]     == 55 );
 
     timepoint = 3;
-    optional_value = convert::to_array_type(timepoint, temporal_array);
+    optional_value = convert::to_non_temporal_type(timepoint, temporal_array);
     CHECK( (*optional_value).size() ==  1 ); 
     CHECK( (*optional_value)[0]     == 55 );
 
     timepoint = 4;
-    optional_value = convert::to_array_type(timepoint, temporal_array);
+    optional_value = convert::to_non_temporal_type(timepoint, temporal_array);
     CHECK_FALSE( optional_value ); 
 }
